8db85ed97300b35bde7403daa9467208b74319d0
ajin5 <ajinmla94@gmail.com>
2024-06-23 00:48:05 +0530

    complete slidebar in dashboard

---
+++ client/src/component/DashSidebar.jsx
@@ -0,0 +1,36 @@
+import { Sidebar } from 'flowbite-react'
+import React, { useEffect, useState } from 'react'
+import { BsArrow90DegRight } from 'react-icons/bs'
+import { FaHospitalUser } from 'react-icons/fa'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useLocation } from 'react-router-dom'
+
+export default function DashSlidebar() {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <Sidebar>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to='/dashboard?tab=profile'>
+          <Sidebar.Item active={tab === 'profile'} icon={FaHospitalUser} label={'user'} labelColor={'dark'}>
+            profile
+          </Sidebar.Item>  
+          </Link>
+          <Sidebar.Item icon={BsArrow90DegRight}>
+            Sign Out
+          </Sidebar.Item>  
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  )
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import {useEffect, useState} from 'react';
+import {useLocation} from 'react-router-dom';
+import DashProfile from '../component/DashProfile';
+import DashSidebar from '../component/DashSidebar';
+
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl){
+      setTab(tabFromUrl);
+    }
+
+  }, [location.search]);
   return (
-    <h1>Dash</h1>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+    <div className=''>
+      {/* sidebar */}
+      <DashSidebar />
+    </div>
+    {/* {profile} */}
+    {tab === 'profile'&& <DashProfile />}
+    </div>
   )
 }
