827c6d0a8bfe2be2efbab40a2f4014317991fe18
ajin5 <ajinmla94@gmail.com>
2024-12-25 17:38:33 +0530

    Error Solved

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import  {create, getposts } from '../controllers/post.controllers.js'
+import  {create, getposts, deletepost } from '../controllers/post.controllers.js'
 import {verifyToken} from '../utils/verifyUser.js'
 
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post('/create', verifyToken, create)
 router.get('/getposts', getposts)
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
 
 export default router
--- client/src/component/DashPost.jsx
+++ client/src/component/DashPost.jsx
@@ -1,12 +1,15 @@
-import { Button, Table } from 'flowbite-react'
+import { Button, Modal, Table } from 'flowbite-react'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { HiOutlineExclamationCircle } from 'react-icons/hi'
 
 export default function DashPost() {
   const { currentUser } = useSelector((state) => state.user)
   const [userPosts, setUserPosts] = useState([]) // Set initial value as an empty array
   const [showMore, setShowMore] = useState(true)
+  const [showModal, setShowModal] = useState(false)
+  const [postIdToDelete, setPostIdToDelete] = useState('')
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -49,6 +52,29 @@ export default function DashPost() {
       console.log(error.message);
 
     }
+  }
+  const handleDeletePost = async()=>{
+    setShowModal(false)
+    try{
+      const res = await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}`,{
+        method:'DELETE',
+
+      });
+      const data = await res.json()
+      if(!res.ok){
+        console.log(data.message)
+
+      }else{
+        setUserPosts((prev)=>
+        prev.filter((post)=>post._id !==postIdToDelete));
+      }
+      
+
+    }catch(error){
+      console.log(error.message)
+
+    }
+
   }
 
   return (
@@ -113,14 +139,7 @@ export default function DashPost() {
               </Table.Body>
             ))}
           </Table>
-          {/* {showMore && (
-            <button
-              onClick={handleShowMore}
-              className='w-full text-teal-500 self-center text-sm py-7'
-            >
-              Show more
-            </button>
-          )} */}
+          
           {showMore && (
             <Button onClick={handleShowMore}
             className='w-full text-cyan-50 self-center text-sm py-7'>
@@ -133,6 +152,27 @@ export default function DashPost() {
       ) : (
         <p>You have no posts yet!</p>
       )}
+      <Modal show={showModal}
+      onClose = {()=>setShowModal(false)}
+      popupsize='md'>
+        <Modal.Header/>
+        <Modal.Body>
+        <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+              Are you sure you want to delete your account?
+            </h3>
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeletePost}>
+                Yes, I'm sure
+              </Button>
+              <Button color='gray' onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   )
 }
