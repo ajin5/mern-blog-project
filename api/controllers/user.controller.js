9c406647216c4ee26e353eecf2c06fc7a72238aa
ajin5 <ajinmla94@gmail.com>
2025-01-01 12:30:50 +0530

    Create get Users API Route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -76,4 +76,39 @@ export const signOut = async(req, res, next) =>{
     next(error);
   }
 }
+
+export const getUsers = async(req, res, next) =>{
+  if(!req.user.isAdmin){
+    return next(errorHandler(400,"You are not Allowed"))
+
+  }
+  try{
+    const startIndex = parseInt(req.query.startIndex || 0)
+    const limit = parseInt(req.query.limit || 9)
+    const sortDirection = req.query.sort === 'asc'? 1: -1;
+    
+    const users = await User.find().sort({createdAt:sortDirection}).skip(startIndex).limit(limit)
+    const usersWithoutPassword = users.map((user)=>{
+      const {password, ...rest} = user._doc
+      return rest;
+    })
+    const totalUsers = await User.countDocuments();
+    const now = new Date();
+    const monthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() -1,
+      now.getDate()
+    )
+    const lastMonthUsers = await User.countDocuments({
+      createdAt:{$gte:monthAgo},
+    })
+    res.status(200).json({
+      users:usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    })
+  }catch(error){
+    next(error)
+  }
+}
   
