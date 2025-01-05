d81f86fb7fc1be78df45bc1fcad9c3ab9729e0d9
ajin5 <ajinmla94@gmail.com>
2025-01-05 23:10:57 +0530

    Changes Updated

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Button, Spinner } from 'flowbite-react';
+import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
 
 export default function PostPage() {
     const {postslug} = useParams()
@@ -67,18 +68,18 @@ return (
         className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
-      <div className='max-w-4xl mx-auto w-full'>
+      {/* <div className='max-w-4xl mx-auto w-full'>
         <CallToAction />
       </div>
-      <CommentSection postId={post._id} />
+      <CommentSection postId={post._id} /> */}
 
-      <div className='flex flex-col justify-center items-center mb-5'>
+      {/* <div className='flex flex-col justify-center items-center mb-5'>
         <h1 className='text-xl mt-5'>Recent articles</h1>
         <div className='flex flex-wrap gap-5 mt-5 justify-center'>
           {recentPosts &&
             recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
         </div>
-      </div>
+      </div> */}
     </main>
   );
 }
